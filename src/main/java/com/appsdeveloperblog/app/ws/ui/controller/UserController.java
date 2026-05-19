8826fe4bd9e76706829a6278c10ee70c07a593e8
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-05-19 08:33:14 +0500

    a

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 	@PostMapping
 	public String createUser()
 	{
-		return "create user was called";  //nurlan as
+		return "create user was called";  //nurlan ashimoff
 	}
 	
 	@PutMapping
