31d19200972d757cf743ae2c6baf1ab2bac5a600
nashimoff <nurlan.ashimoff@gmail.com>
2026-05-20 00:00:02 +0600

    a

---
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -0,0 +1,42 @@
+package com.appsdeveloperblog.app.ws.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.appsdeveloperblog.app.ws.service.UserService;
+
+@EnableWebSecurity
+public class WebSecurity {
+	
+	private final UserService userDetailsService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userDetailsService = userSer
+	}
+	
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+	
+	// Configure AuthenticationManagerBuilder
+	AuthenticationManagerBuilder authenticationManagerBuilder = 	
+			http.getSharedObject(AuthenticationManagerBuilder.class);
+	
+	authenticationManagerBuilder
+	.userDetailsService(userDetailsService)
+	.passwordEncoder(bCryptPasswordEncoder);
+	
+	http.csrf().disable()
+	.authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll()
+	.anyRequest().authenticated();
+	
+}
+	
+}
+	
+	
--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -2,11 +2,15 @@ package com.appsdeveloperblog.app.ws.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.appsdeveloperblog.app.ws.UserRepository;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
 @Service
@@ -14,15 +18,25 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	Utils utils;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public UserDto createUser(UserDto user) {
 		
+		if( userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+		
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
-		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserId");
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
@@ -30,6 +44,12 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
 		
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
