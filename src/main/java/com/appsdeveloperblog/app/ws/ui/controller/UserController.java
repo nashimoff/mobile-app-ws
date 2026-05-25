92c42bb624b6a51083ce7efcc7f23546c52bb233
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-05-25 15:42:50 +0500

    a

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -49,11 +50,14 @@ public class UserController {
 	{
 		UserRest returnValue = new UserRest();
 		
-		UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails,userDto);
+//		UserDto userDto = new UserDto();
+//		BeanUtils.copyProperties(userDetails,userDto);
+		
+		ModelMapper  modelMapper = new ModelMapper();
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 		
 		UserDto createdUser = userService.createUser(userDto);
-		BeanUtils.copyProperties(createdUser, returnValue);
+		returnValue = modelMapper.map(createdUser, UserRest.class);
 		
 		return returnValue;
 		
@@ -68,6 +72,7 @@ public class UserController {
 		
 		UserDto userDto = new UserDto();
 		BeanUtils.copyProperties(userDetails,userDto);
+	
 		
 		UserDto updateUser = userService.updateUser(id, userDto);
 		BeanUtils.copyProperties(updateUser, returnValue);
