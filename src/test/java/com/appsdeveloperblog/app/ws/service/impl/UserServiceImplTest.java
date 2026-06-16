5411179f2cd67f8670ffa8c720afccd74d397552
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-06-16 16:21:26 +0500

    a

--- src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
@@ -5,22 +5,36 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.io.repository.UserRepository;
 
 class UserServiceImplTest {
+	
+	@InjectMocks
+	UserServiceImpl userService;
 	
 	@Mock
 	UserRepository userRepository;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
 	final void testGetUser() {
-		when(userRepository.findByEmail(anyString())).thenReturn(null);
+		
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setFirstName("Sergey");
+		userEntity.setUserId("hhty57ehfy");
+		userEntity.setEncryptedPassword("74hghd8474jf");
+		
+		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
 	}
 
 }
