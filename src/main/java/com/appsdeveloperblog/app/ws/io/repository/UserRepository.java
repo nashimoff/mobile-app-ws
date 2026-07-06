9e3c4498f0835d93ae2be1f878881b7a67745655
nashimoff <nurlan.ashimoff@gmail.com>
2026-07-06 20:35:20 +0600

    a

--- src/main/java/com/appsdeveloperblog/app/ws/io/repository/UserRepository.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
@@ -23,4 +24,7 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
 	
 	@Query(value="select * from Users u where u.first_name = ?1", nativeQuery=true)
 	List<UserEntity> findUserByFirstName(String firstName);
+	
+	@Query(value="select * from Users u where u.last_name = :lastName", nativeQuery=true)
+	List<UserEntity> findUserByLastName(@Param("lastName") String lastName);
 }
