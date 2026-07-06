ee5dff1bdac7d9df6a38c8640c17f1ffcebd2360
nashimoff <nurlan.ashimoff@gmail.com>
2026-07-06 20:41:55 +0600

    a

--- src/test/java/com/appsdeveloperblog/app/ws/io/repository/UserRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/io/repository/UserRepositoryTest.java
@@ -56,7 +56,7 @@ class UserRepositoryTest {
 	@Test
 	final void testFindUserByLastName()
 	{
-		String lastName="Sergey";
+		String lastName="Kargopolov";
 		List<UserEntity> users = userRepository.findUserByFirstName(firstName);
 		assertNotNull(users);
 		assertTrue(users.size() == 2);
