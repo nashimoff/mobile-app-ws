1774debb2834c427df4508325c1fff66711d2e21
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-07-10 16:16:29 +0500

    a

--- src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
+++ src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
@@ -12,6 +12,9 @@ import org.springframework.plugin.core.SimplePluginRegistry;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,21 +23,32 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	Contact contact = new Contact(
+			"Sergey Kargopolov",
+			"http://www.appsdeveloper.blog.com",
+			"developer@appsdeveloperblog.com"
+			);
+	
+	List<VendorExtension> vendorExtensions = new ArrayList<>();
+
+	ApiInfo apiInfo = new ApiInfo("Photo app RESTful Web Service documentation",
+			"This pages documents Photo app RESTful Web Service endpoints", "1.0",
+			"http://www.appsdeveloperblog.com/service.html", contact, "Apache 2.0",
+			"http://www.apache.org/licenses/LICENSE-2.0", vendorExtensions);
+
 	@Bean
 	public Docket apiDocket() {
-		
-		Docket docket = new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.app.ws"))
-				.paths(PathSelectors.any())
+
+		Docket docket = new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.app.ws")).paths(PathSelectors.any())
 				.build();
-		
+
 		return docket;
 	}
-	
+
 	@Bean
 	public LinkDiscoverers discovers() {
-		
+
 		List<LinkDiscoverer> plugins = new ArrayList<>();
 		plugins.add(new CollectionJsonLinkDiscoverer());
 		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
