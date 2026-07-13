d1e1bda191ee6f63ad1d495a395cc66d58cc761b
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-07-13 14:25:58 +0500

    a

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
-import java.util.ArrayList; //////nurlan
+import java.util.ArrayList; //////nur
 import java.util.Arrays;
 import java.util.List;
 
