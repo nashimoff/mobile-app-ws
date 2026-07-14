0156eab94e6cb6ed54c380f54871a83bd53e4ab2
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-07-14 08:52:36 +0600

    a

--- src/main/java/com/appsdeveloperblog/app/ws/io/repository/AuthorityRepository.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repository/AuthorityRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.appsdeveloperblog.app.ws.io.entity.AuthorityEntity;
 
 public interface AuthorityRepository extends CrudRepository<AuthorityEntity, Long> {
+	AuthorityEntity findByName(String name);
 
 }
