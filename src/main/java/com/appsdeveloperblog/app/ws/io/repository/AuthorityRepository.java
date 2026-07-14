469e48b797e1bcf7fe1012743a67b5f9580aa4bc
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-07-14 08:47:21 +0600

    a

---
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.app.ws.io.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.appsdeveloperblog.app.ws.io.entity.AuthorityEntity;
+
+public interface AuthorityRepository extends CrudRepository<AuthorityEntity, Long> {
+
+}
